7b8de05fe21427366a512b61ba35d4405e9490a2
sky828 <667515@qq.com>
2025-05-21 09:44:31 +0800

    Update debian-hy.sh

--- debian-hy.sh
+++ debian-hy.sh
@@ -18,7 +18,7 @@ if [ -f /etc/hysteria/config.yaml ]; then
     KEY_PATH=$(grep -E '^\s+key:' /etc/hysteria/config.yaml | awk '{print $2}')
 else
     echo "未找到 /etc/hysteria/config.yaml 文件，请检查是否安装了hy2"
-    echo "适用 debian 的hy脚本："
+    echo "适用 debian 的hy2脚本："
     echo "wget -N --no-check-certificate https://raw.githubusercontent.com/Misaka-blog/hysteria-install/main/hy2/hysteria.sh && bash hysteria.sh"
     exit 1
 fi
